50c21447daa1fd04e648023b3e6d872b0130f8cb
Hendra Anggrian <hendraanggrian@gmail.com>
2017-09-22 04:58:21 +0700

    fixes bindingOf

--- src/main/kotlin/kotfx/bindings/bindingOf.kt
+++ src/main/kotlin/kotfx/bindings/bindingOf.kt
@@ -9,22 +9,22 @@ import javafx.beans.binding.*
 import java.util.concurrent.Callable
 
 /** Helper function to create a custom [BooleanBinding]. */
-inline fun bindingOf(vararg dependencies: Observable, noinline func: () -> Boolean): BooleanBinding = Bindings.createBooleanBinding(Callable { func() }, *dependencies)
+inline fun booleanBindingOf(vararg dependencies: Observable, noinline func: () -> Boolean): BooleanBinding = Bindings.createBooleanBinding(Callable { func() }, *dependencies)
 
 /** Helper function to create a custom [DoubleBinding]. */
-inline fun bindingOf(vararg dependencies: Observable, noinline func: () -> Double): DoubleBinding = Bindings.createDoubleBinding(Callable { func() }, *dependencies)
+inline fun doubleBindingOf(vararg dependencies: Observable, noinline func: () -> Double): DoubleBinding = Bindings.createDoubleBinding(Callable { func() }, *dependencies)
 
 /** Helper function to create a custom [FloatBinding]. */
-inline fun bindingOf(vararg dependencies: Observable, noinline func: () -> Float): FloatBinding = Bindings.createFloatBinding(Callable { func() }, *dependencies)
+inline fun floatBindingOf(vararg dependencies: Observable, noinline func: () -> Float): FloatBinding = Bindings.createFloatBinding(Callable { func() }, *dependencies)
 
 /** Helper function to create a custom [IntegerBinding]. */
-inline fun bindingOf(vararg dependencies: Observable, noinline func: () -> Int): IntegerBinding = Bindings.createIntegerBinding(Callable { func() }, *dependencies)
+inline fun integerBindingOf(vararg dependencies: Observable, noinline func: () -> Int): IntegerBinding = Bindings.createIntegerBinding(Callable { func() }, *dependencies)
 
 /** Helper function to create a custom [LongBinding]. */
-inline fun bindingOf(vararg dependencies: Observable, noinline func: () -> Long): LongBinding = Bindings.createLongBinding(Callable { func() }, *dependencies)
+inline fun longBindingOf(vararg dependencies: Observable, noinline func: () -> Long): LongBinding = Bindings.createLongBinding(Callable { func() }, *dependencies)
 
 /** Helper function to create a custom [ObjectBinding]. */
 inline fun <T> bindingOf(vararg dependencies: Observable, noinline func: () -> T): ObjectBinding<T> = Bindings.createObjectBinding(Callable { func() }, *dependencies)
 
 /** Helper function to create a custom [StringBinding]. */
-inline fun bindingOf(vararg dependencies: Observable, noinline func: () -> String): StringBinding = Bindings.createStringBinding(Callable { func() }, *dependencies)
+inline fun stringBindingOf(vararg dependencies: Observable, noinline func: () -> String): StringBinding = Bindings.createStringBinding(Callable { func() }, *dependencies)

a460b53cb6750c903ff7afd365d94401c5f43b7d
Hendra Anggrian <hendraanggrian@gmail.com>
2018-03-18 02:23:47 +0700

    Add simpler file and directory choosers

--- kfx-commons/src/kfx/stage/DirectoryChooser.kt
+++ kfx-commons/src/kfx/stage/DirectoryChooser.kt
@@ -7,6 +7,15 @@ import javafx.stage.DirectoryChooser
 /**
  * Build a directory chooser dialog with Kotlin DSL.
  *
+ * @param init custom initialization block.
+ */
+inline fun directoryChooser(
+    noinline init: (DirectoryChooser.() -> Unit)? = null
+): DirectoryChooser = directoryChooser(null, init)
+
+/**
+ * Build a directory chooser dialog with Kotlin DSL.
+ *
  * @param title title of the dialog.
  * @param init custom initialization block.
  */
--- kfx-commons/src/kfx/stage/FileChooser.kt
+++ kfx-commons/src/kfx/stage/FileChooser.kt
@@ -7,6 +7,17 @@ import javafx.stage.FileChooser
 /**
  * Build a file chooser dialog with Kotlin DSL.
  *
+ * @param filters expected file extensions.
+ * @param init custom initialization block.
+ */
+inline fun fileChooser(
+    vararg filters: FileChooser.ExtensionFilter,
+    noinline init: (FileChooser.() -> Unit)? = null
+): FileChooser = fileChooser(null, *filters, init = init)
+
+/**
+ * Build a file chooser dialog with Kotlin DSL.
+ *
  * @param title title of the dialog.
  * @param filters expected file extensions.
  * @param init custom initialization block.

f380c6a8511a53081753f3f7e9727adcc60ccbdf
Hendra Anggrian <hendraanggrian@gmail.com>
2018-04-06 05:11:18 +0700

    String invocation in contextMenu

--- ktfx-layouts/src/ktfx/layouts/scene/control/ContextMenu.kt
+++ ktfx-layouts/src/ktfx/layouts/scene/control/ContextMenu.kt
@@ -3,6 +3,7 @@
 package ktfx.layouts
 
 import javafx.collections.ObservableList
+import javafx.scene.Node
 import javafx.scene.control.ContextMenu
 import javafx.scene.control.Control
 import javafx.scene.control.MenuItem
@@ -12,6 +13,15 @@ import javafx.scene.control.TableColumnBase
 open class _ContextMenu(vararg items: MenuItem) : ContextMenu(*items), LayoutManager<MenuItem> {
 
     override val childs: ObservableList<MenuItem> get() = items
+
+    /** Convenient method to add item to this menu by only using string. */
+    inline operator fun String.invoke(graphic: Node? = null): MenuItem = invoke(graphic) { }
+
+    /** Convenient method to add item to this menu by only using string. */
+    inline operator fun String.invoke(
+        graphic: Node? = null,
+        init: (@LayoutDsl MenuItem).() -> Unit
+    ): MenuItem = menuItem(this, graphic, init)
 }
 
 inline fun contextMenu(

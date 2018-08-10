057daa8d80363ab77af058e8610939649f7cfaae
Hendra Anggrian <hendraanggrian@gmail.com>
2018-08-11 01:37:21 +0700

    Internal dependency update

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,5 @@
-ARTIFACTS.forEach {
-    include(":$it")
-}
-
-ARTIFACTS_THIRDPARTY.forEach {
-    include(":$it")
-}
+ARTIFACTS.forEach { include(":$it") }
+ARTIFACTS_THIRDPARTY.forEach { include(":$it") }
 
 include(":testing")
-
 include(":demo")

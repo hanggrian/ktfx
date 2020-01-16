6053a3ae99b6abe75d89c2607da95d3622fac339
Hendra Anggrian <hendraanggrian@gmail.com>
2020-01-17 03:01:40 +0700

    More layout gap extensions

--- ktfx-commons/src/ktfx/controls/Layout.kt
+++ ktfx-commons/src/ktfx/controls/Layout.kt
@@ -8,25 +8,34 @@ import javafx.scene.layout.TilePane
 import ktfx.internal.KtfxInternals
 
 /** Sets horizontal and vertical gap. */
+inline fun FlowPane.setGap(hgap: Double, vgap: Double) {
+    this.hgap = hgap
+    this.vgap = vgap
+}
+
+/** Convenient method to set horizontal and vertical gap to the same value. */
 inline var FlowPane.gap: Double
     @Deprecated(KtfxInternals.NO_GETTER, level = DeprecationLevel.ERROR) get() = KtfxInternals.noGetter()
-    set(value) {
-        vgap = value
-        hgap = value
-    }
+    set(value) = setGap(value, value)
 
-/** Sets a horizontal and vertical gap. */
+/** Sets horizontal and vertical gap. */
+inline fun GridPane.setGap(hgap: Double, vgap: Double) {
+    this.hgap = hgap
+    this.vgap = vgap
+}
+
+/** Convenient method to set horizontal and vertical gap to the same value. */
 inline var GridPane.gap: Double
     @Deprecated(KtfxInternals.NO_GETTER, level = DeprecationLevel.ERROR) get() = KtfxInternals.noGetter()
-    set(value) {
-        vgap = value
-        hgap = value
-    }
+    set(value) = setGap(value, value)
 
-/** Sets a horizontal and vertical gap. */
+/** Sets horizontal and vertical gap. */
+inline fun TilePane.setGap(hgap: Double, vgap: Double) {
+    this.hgap = hgap
+    this.vgap = vgap
+}
+
+/** Convenient method to set horizontal and vertical gap to the same value. */
 inline var TilePane.gap: Double
     @Deprecated(KtfxInternals.NO_GETTER, level = DeprecationLevel.ERROR) get() = KtfxInternals.noGetter()
-    set(value) {
-        vgap = value
-        hgap = value
-    }
+    set(value) = setGap(value, value)

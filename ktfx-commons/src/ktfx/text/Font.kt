3ac8092b5316e4326c45f0aef20929cd684dc297
Hendra Anggrian <hendraanggrian@gmail.com>
2020-04-07 14:01:34 +0700

    `plus` operators replaces `fontOf` and `italicFontOf`

--- ktfx-commons/src/ktfx/text/Font.kt
+++ ktfx-commons/src/ktfx/text/Font.kt
@@ -2,7 +2,6 @@
 
 package ktfx.text
 
-import java.io.InputStream
 import javafx.scene.text.Font
 import javafx.scene.text.FontPosture
 import javafx.scene.text.FontWeight
@@ -13,30 +12,82 @@ val Int.pt: Font get() = Font.font(toDouble())
 /** Returns a default font with size of this [Double] number. */
 inline val Double.pt: Font get() = Font.font(this)
 
-/** Returns a font with specified [family], [weight] and [size] of an [Int] number. */
-fun fontOf(family: String? = null, weight: FontWeight? = null, size: Int): Font =
-    Font.font(family, weight, size.toDouble())
+/** Compose a font of [family], [this] weight, default posture, and default size. */
+operator fun FontWeight.plus(family: String): Font = Font.font(family, this, 12.0)
 
-/** Returns a font with specified [family], [weight] and [size] of a [Double] number. */
-inline fun fontOf(family: String? = null, weight: FontWeight? = null, size: Double = -1.0): Font =
-    Font.font(family, weight, size)
+/** Compose a font of default family, [this] weight, [posture], and default size. */
+operator fun FontWeight.plus(posture: FontPosture): Font = Font.font(Font.getDefault().family, this, posture, 12.0)
 
-/** Returns an italic font with specified [family], [weight] and [size] of an [Int] number. */
-fun italicFontOf(family: String? = null, weight: FontWeight? = null, size: Int): Font =
-    Font.font(family, weight, FontPosture.ITALIC, size.toDouble())
+/** Compose a font of default family, [this] weight, default posture, and [size]. */
+operator fun FontWeight.plus(size: Double): Font = Font.font(Font.getDefault().family, this, size)
 
-/** Returns an italic font with specified [family], [weight] and [size] of a [Double] number. */
-inline fun italicFontOf(family: String? = null, weight: FontWeight? = null, size: Double = -1.0): Font =
-    Font.font(family, weight, FontPosture.ITALIC, size)
+/** Compose a font of default family, [this] weight, default posture, and [size]. */
+operator fun FontWeight.plus(size: Int): Font = Font.font(Font.getDefault().family, this, size.toDouble())
 
-/** Load a font from [url], specifying [size] of an [Int] number. */
-fun loadFont(url: String, size: Int): Font = Font.loadFont(url, size.toDouble())
+/** Compose a font of [family], default weight, [this] posture, and default size. */
+operator fun FontPosture.plus(family: String): Font = Font.font(family, this, 12.0)
 
-/** Load a font from [url], specifying [size] of a [Double] number. */
-inline fun loadFont(url: String, size: Double = 0.0): Font = Font.loadFont(url, size)
+/** Compose a font of default family, [weight], [this] posture, and default size. */
+operator fun FontPosture.plus(weight: FontWeight): Font = Font.font(Font.getDefault().family, weight, this, 12.0)
 
-/** Load a font from [stream], specifying [size] of an [Int] number. */
-fun loadFont(stream: InputStream, size: Int): Font = Font.loadFont(stream, size.toDouble())
+/** Compose a font of default family, default weight, [this] posture, and [size]. */
+operator fun FontPosture.plus(size: Double): Font = Font.font(Font.getDefault().family, this, size)
 
-/** Load a font from [stream], specifying [size] of a [Double] number. */
-inline fun loadFont(stream: InputStream, size: Double = 0.0): Font = Font.loadFont(stream, size)
+/** Compose a font of default family, default weight, [this] posture, and [size]. */
+operator fun FontPosture.plus(size: Int): Font = Font.font(Font.getDefault().family, this, size.toDouble())
+
+/** Compose a font of [family], default weight, default posture, and [this] size. */
+operator fun Double.plus(family: String): Font = Font.font(family, this)
+
+/** Compose a font of default family, [weight], default posture, and [this] size. */
+operator fun Double.plus(weight: FontWeight): Font = Font.font(Font.getDefault().family, weight, this)
+
+/** Compose a font of default family, default weight, [posture], and [this] size. */
+operator fun Double.plus(posture: FontPosture): Font = Font.font(Font.getDefault().family, posture, this)
+
+/** Compose a font of [family], default weight, default posture, and [this] size. */
+operator fun Int.plus(family: String): Font = Font.font(family, toDouble())
+
+/** Compose a font of default family, [weight], default posture, and [this] size. */
+operator fun Int.plus(weight: FontWeight): Font = Font.font(Font.getDefault().family, weight, toDouble())
+
+/** Compose a font of default family, default weight, [posture], and [this] size. */
+operator fun Int.plus(posture: FontPosture): Font = Font.font(Font.getDefault().family, posture, toDouble())
+
+/** Compose a font of [family], [this] font's weight, [this] font's posture, and [this] font's size. */
+operator fun Font.plus(family: String): Font = Font.font(family, weight, posture, size)
+
+/** Compose a font of [this] font's family, [weight], [this] font's posture, and [this] font's size. */
+operator fun Font.plus(weight: FontWeight): Font = Font.font(family, weight, posture, size)
+
+/** Compose a font of [this] font's family, [this] font's weight, [posture], and [this] font's size. */
+operator fun Font.plus(posture: FontPosture): Font = Font.font(family, weight, posture, size)
+
+/** Compose a font of [this] font's family, [this] font's weight, [this] font's posture, and [size]. */
+operator fun Font.plus(size: Double): Font = Font.font(family, weight, posture, size)
+
+/** Compose a font of [this] font's family, [this] font's weight, [this] font's posture, and [size]. */
+operator fun Font.plus(size: Int): Font = Font.font(family, weight, posture, size.toDouble())
+
+internal val Font.weight: FontWeight
+    get() = when {
+        "Light" in style -> when {
+            "Extra" in style || "Ultra" in style -> FontWeight.EXTRA_LIGHT
+            else -> FontWeight.LIGHT
+        }
+        "Bold" in style -> when {
+            "Semi" in style || "Demi" in style -> FontWeight.SEMI_BOLD
+            "Extra" in style || "Ultra" in style -> FontWeight.EXTRA_BOLD
+            else -> FontWeight.BOLD
+        }
+        "Black" in style || "Heavy" in style -> FontWeight.BLACK
+        "Thin" in style -> FontWeight.THIN
+        "Medium" in style -> FontWeight.MEDIUM
+        else -> FontWeight.NORMAL
+    }
+
+internal val Font.posture: FontPosture
+    get() = when {
+        "Italic" in style -> FontPosture.ITALIC
+        else -> FontPosture.REGULAR
+    }
--- ktfx-commons/tests/src/ktfx/text/FontTest.kt
+++ ktfx-commons/tests/src/ktfx/text/FontTest.kt
@@ -1,17 +1,81 @@
 package ktfx.text
 
 import javafx.scene.text.Font
+import javafx.scene.text.FontPosture
+import javafx.scene.text.FontWeight
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
-/**
- * [ktfx.text.fontOf] and [ktfx.text.loadFont] is not being tested,
- * since it seems to give different result on different OS.
- */
 class FontTest {
 
     @Test fun pt() {
         assertEquals(Font.font(18.0), 18.0.pt)
         assertEquals(Font.font(12.0), 12.pt)
+    }
+
+    @Test fun plus() {
+        val boldArial = FontWeight.BOLD + "Arial"
+        assertTrue("Bold" in boldArial.name)
+        assertTrue("Arial" in boldArial.name)
+        val boldItalic = FontWeight.BOLD + FontPosture.ITALIC
+        assertTrue("Bold" in boldItalic.name)
+        assertTrue("Italic" in boldItalic.name)
+        val bold16 = FontWeight.BOLD + 16.0
+        assertTrue("Bold" in bold16.name)
+        assertEquals(16.0, bold16.size)
+        val bold18 = FontWeight.BOLD + 18
+        assertTrue("Bold" in bold18.name)
+        assertEquals(18.0, bold18.size)
+
+        val italicArial = FontPosture.ITALIC + "Arial"
+        assertTrue("Italic" in italicArial.name)
+        assertTrue("Arial" in italicArial.name)
+        val italicBold = FontPosture.ITALIC + FontWeight.BOLD
+        assertTrue("Italic" in italicBold.name)
+        assertTrue("Bold" in italicBold.name)
+        val italic16 = FontPosture.ITALIC + 16.0
+        assertTrue("Italic" in italic16.name)
+        assertEquals(16.0, italic16.size)
+        val italic18 = FontPosture.ITALIC + 18
+        assertTrue("Italic" in italic18.name)
+        assertEquals(18.0, italic18.size)
+
+        val doubleArial = 16.0 + "Arial"
+        assertEquals(16.0, doubleArial.size)
+        assertTrue("Arial" in doubleArial.name)
+        val doubleBold = 16.0 + FontWeight.BOLD
+        assertEquals(16.0, doubleBold.size)
+        assertTrue("Bold" in doubleBold.name)
+        val doubleItalic = 16.0 + FontPosture.ITALIC
+        assertEquals(16.0, doubleItalic.size)
+        assertTrue("Italic" in doubleItalic.name)
+
+        val intArial = 18 + "Arial"
+        assertEquals(18.0, intArial.size)
+        assertTrue("Arial" in intArial.name)
+        val intBold = 18 + FontWeight.BOLD
+        assertEquals(18.0, intBold.size)
+        assertTrue("Bold" in intBold.name)
+        val intItalic = 18 + FontPosture.ITALIC
+        assertEquals(18.0, intItalic.size)
+        assertTrue("Italic" in intItalic.name)
+
+        val system = Font.getDefault()
+        val arialBoldItalic21 = system + "Arial" + FontWeight.BOLD + FontPosture.ITALIC + 21.0
+        assertTrue("Arial" in arialBoldItalic21.name)
+        assertTrue("Bold" in arialBoldItalic21.name)
+        assertTrue("Italic" in arialBoldItalic21.name)
+        assertEquals(21.0, arialBoldItalic21.size)
+        val arialBoldItalic24 = system + "Arial" + FontWeight.BOLD + FontPosture.ITALIC + 24
+        assertTrue("Arial" in arialBoldItalic24.name)
+        assertTrue("Bold" in arialBoldItalic24.name)
+        assertTrue("Italic" in arialBoldItalic24.name)
+        assertEquals(24.0, arialBoldItalic24.size)
+    }
+
+    @Test fun internal() {
+        assertEquals(FontWeight.BOLD, (FontWeight.BOLD + 14).weight)
+        assertEquals(FontPosture.ITALIC, (FontPosture.ITALIC + 14).posture)
     }
 }
